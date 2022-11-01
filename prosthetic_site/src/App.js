167324a1f3aafd17bec79c243f211633cd49f2a0
tamir057 <tas.amir57@gmail.com>
2022-11-01 14:50:36 -0400

    layout updates

--- prosthetic_site/src/App.js
+++ prosthetic_site/src/App.js
@@ -1,28 +1,34 @@
 import logo from './logo.svg';
 //import Webimage from "./WebImage"
-
 import './App.css';
 import React, {Component} from 'react';
 import NavBar from './navbar';
-
 
 function App() {
   return (
     <div className="App"> 
     <NavBar/>
       <div className="App-header">
-        <p>
-          Upper Limb Prosthetic Guide   
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
+        <p style={{
+          fontSize: "150px;"
+          }}>
+          Upper Limb Prosthetic Guide
+          </p> 
+      </div>
+      <div className="App"> 
+      <div className="App-bg2">
+        <p className='subtitles'>
+          Preparing For Your First Visit
+          </p> 
+          <p>
+          </p>
+          <p className="textBody">
+          Don’t be afraid to ask questions! Your prosthetist will ask you questions about your lifestyle and goals for your prosthesis.
+          Have a list of goals ready to share with your prosthetist. 
+          </p>
+          <p  className="textBody">What are you planning to use your prosthesis for? What are some aspects that you value in your prosthesis (i.e appearance, functionality, durability, etc.)
+          Be aware that the process can be lengthy and it will take several visits before you get to try on a prosthetic socket.
+          </p> 
       </div>
       <div className="App-header">
         <span className="Controls">
@@ -33,8 +39,9 @@ function App() {
         <textarea rows="5" className="Text" />
       </div>
     </div>
+    </div>
+    
+    
   );
 }
-
 export default App;
-
