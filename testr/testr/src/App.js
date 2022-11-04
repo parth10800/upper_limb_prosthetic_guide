c22bbbc968c41bd40211974067d8f6e5dbef9315
Rseelan <seelan.r@husky.neu.edu>
2022-11-04 19:05:03 -0400

    1st iteration at google maps api

---
+++ testr/testr/src/App.js
@@ -0,0 +1,62 @@
+/*
+import logo from './logo.svg';
+import './App.css';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          chalk
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+*/
+
+import React from 'react'
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+function MyComponent() {
+  return (
+    <LoadScript
+      googleMapsApiKey="AIzaSyAg3gyl1n_zcZDvmG9CREbN2h4bxbBsxA4"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
+}
+
+export default React.memo(MyComponent)
