d5268c7c1269251fbc9ea343aedf2dd3f0fb25f1
tamir057 <tas.amir57@gmail.com>
2022-11-04 14:20:02 -0400

    more layouts

--- prosthetic_site/src/App.js
+++ prosthetic_site/src/App.js
@@ -1,42 +1,63 @@
 import logo from './logo.svg';
 //import Webimage from "./WebImage"
 import './App.css';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import NavBar from './navbar';
 
 
 function App() {
   return (
-    <div className="App"> 
-    <NavBar/>
+    <div className="App">
+      <NavBar />
       <div className="App-header">
         <p style={{
           fontSize: "150px;"
-          }}>
+        }}>
           Upper Limb Prosthetic Guide
-          </p> 
+        </p>
       </div>
-      <div className="App"> 
-      <div className="App-bg2">
-        <p className='subtitles'>
-          Preparing For Your First Visit
-          </p> 
+      <div className="App">
+        <div className="App-bg2">
+          <p className='subtitles'>
+            Preparing For Your First Visit
+          </p>
           <p>
           </p>
           <p className="textBody">
-          Don’t be afraid to ask questions! Your prosthetist will ask you questions about your lifestyle and goals for your prosthesis.
+            Don’t be afraid to ask questions! Your prosthetist will ask you questions about your lifestyle and goals for your prosthesis.
           </p>
           <p className="textBody">
-          Have a list of goals ready to share with your prosthetist. What are you planning to use your prosthesis for? 
+            Have a list of goals ready to share with your prosthetist. What are you planning to use your prosthesis for?
           </p>
           <p className="textBody">What are some aspects that you value in your prosthesis (i.e appearance, functionality, durability, etc.)
-          Be aware that the process can be lengthy and it will take several visits before you get to try on a prosthetic socket.
-          </p> 
+            Be aware that the process can be lengthy and it will take several visits before you get to try on a prosthetic socket.
+          </p>
+        </div>
+        <div className="App-bg2">
+          <p className='subtitles'>
+            About Us
+          </p>
+          <p>
+          </p>
+          <p className="textBody">
+            Our Goal
+          </p>
+          <p className="textBody">
+            Welcome to the upper limb prosthetic guide. Our goal is to provide a central platform to introduce those with upper limb loss into the complex world of prosthetics. We hope this website provides some helpful information to guide you towards your first prosthesis.
+          </p>
+          <p className="textBody">
+            Who are we?
+          </p>
+          <p className="textBody">
+            We are a group of students from Northeastern University. With the help of experts in the field, we have collected the basic information about getting your first prosthesis along with a comparison tool for the current devices in the market.
+          </p>
+
+        </div>
+
       </div>
     </div>
-    </div>
-    
-    
+
+
   );
 }
 export default App;
---
+++ prosthetic_site/src/component/find_prosthetist/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import NavBar from './navbar';
+
+
+function App() {
+    return (
+      <div className="App">
+        <NavBar />
+        <div className="App">
+          <div className="App-bg2">
+            <p className='subtitles'>
+            Do you have a prosthetist yet?
+            </p>
+            <p>
+            </p>
+            <p className="textBody">
+              Find a prosthetist near you.
+            </p>
+            <p className="textBody">
+              Have a list of goals ready to share with your prosthetist. What are you planning to use your prosthesis for?
+            </p>
+            <p className="textBody">What are some aspects that you value in your prosthesis (i.e appearance, functionality, durability, etc.)
+              Be aware that the process can be lengthy and it will take several visits before you get to try on a prosthetic socket.
+            </p>
+          </div>
+          <div className="App-bg2">
+            <p className='subtitles'>
+              About Us
+            </p>
+            <p>
+            </p>
+            <p className="textBody">
+              Our Goal
+            </p>
+            <p className="textBody">
+              Welcome to the upper limb prosthetic guide. Our goal is to provide a central platform to introduce those with upper limb loss into the complex world of prosthetics. We hope this website provides some helpful information to guide you towards your first prosthesis.
+            </p>
+            <p className="textBody">
+              Who are we?
+            </p>
+            <p className="textBody">
+              We are a group of students from Northeastern University. With the help of experts in the field, we have collected the basic information about getting your first prosthesis along with a comparison tool for the current devices in the market.
+            </p>
+  
+          </div>
+  
+        </div>
+      </div>
+  
+  
+    );
+  }
+  export default App;
+  
+
