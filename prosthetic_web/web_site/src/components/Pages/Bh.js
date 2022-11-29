eb3e054d066dcd70a1ca22a53ee5adf44c064f4f
tamir057 <tas.amir57@gmail.com>
2022-11-28 22:57:49 -0500

    comparison tool templatE

--- prosthetic_web/web_site/src/components/Pages/Bh.js
+++ prosthetic_web/web_site/src/components/Pages/Bh.js
@@ -3,15 +3,77 @@ import React from "react";
 
 export const Bh = () => {
   return (
-    <div >
-      <p className='subtitles'>
-            Insert Compare Tool
-          </p>
-          <p>
-          </p>
-          <p className="textBody">
-          Compare tool for bionic hands 
-          </p>
-    </div> 
+    <div>
+      <p class = "subtitle">
+      Bionic Hands
+      </p>
+    <div id="etdRow">
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    </div>
+    </div>
   );
 };
--- prosthetic_web/web_site/src/components/Pages/Etd.js
+++ prosthetic_web/web_site/src/components/Pages/Etd.js
@@ -1,19 +1,165 @@
 import React from "react";
 
+
 export const Etd = () => {
   return (
-    <div >
-      <p className='subtitles'>
-          Electric Terminal Devices (ETDs)
-          </p>
-          <p>
-          <box type="etdBox" name="formBtn" id="btnCompare">
-            Electric Terminal Devices (ETDs)
-            </box>
-          </p>
-          <p className="textBody">
-          Compare tool boxes
-          </p>
-    </div> 
+    <div>
+      <p class = "subtitle">
+      Electric Terminal Devices (ETDs)
+      </p>
+    <div id="etdRow">
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    <div id="etdInfoBox">
+        <div class="col">
+            <p class = "textBody">
+                Hello Test
+            </p>
+            <p class = "textBody">
+                Hello Test 2
+            </p>
+            <p class = "textBody">
+                Hello Test 3
+            </p>
+        </div>
+    </div>
+    </div>
+    </div>
   );
 };
+
+/*
+<style>
+    .row {
+  display: flex; 
+  margin: 0 auto;
+  justify-content: space-evenly;
+}
+#infobox {
+  border-radius: 25px;
+  border: 2px solid #1e0da2;
+  padding: 10px;
+  width: 200px;
+  height: 150px;
+  margin: 15px;
+}
+    </style>
+
+<div class="row">
+<div id="infobox">
+    <div class="col-md-10">
+        <p>
+            Hello Test
+        </p>
+        <p>
+            Hello Test 2
+        </p>
+        <p>
+            Hello Test 3
+        </p>
+    </div>
+</div>
+<div id="infobox">
+    <div class="col-md-10">
+        <p>
+            Hello Test
+        </p>
+        <p>
+            Hello Test 2
+        </p>
+        <p>
+            Hello Test 3
+        </p>
+    </div>
+</div>
+<div id="infobox">
+    <div class="col-md-10">
+        <p>
+            Hello Test
+        </p>
+        <p>
+            Hello Test 2
+        </p>
+        <p>
+            Hello Test 3
+        </p>
+    </div>
+</div>
+<div id="infobox">
+    <div class="col-md-10">
+        <p>
+            Hello Test
+        </p>
+        <p>
+            Hello Test 2
+        </p>
+        <p>
+            Hello Test 3
+        </p>
+    </div>
+</div>
+<div id="infobox">
+    <div class="col-md-10">
+        <p>
+            Hello Test
+        </p>
+        <p>
+            Hello Test 2
+        </p>
+        <p>
+            Hello Test 3
+        </p>
+    </div>
+</div>
+</div>
+*/
